bd766081325d6c12ef4ccdfcff014e67868f6742
s52843 <s52843@edu.campus02.at>
2025-11-22 11:13:04 +0100

    work in progress 5

--- ledController/src/main/java/at/edu/c02/ledcontroller/ApiService.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/ApiService.java
@@ -7,4 +7,5 @@ import java.io.IOException;
 public interface ApiService {
     JSONObject getLights() throws IOException;
     JSONObject getLight(int id) throws IOException;
+    JSONObject setLight(int id, String color, boolean status) throws IOException;
 }
